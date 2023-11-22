142c38081bb61fa14e5af6899d4917a338360dc5
Adam McNamara <20092433@mail.wit.ie>
2023-11-22 16:24:19 +0000

    Bug fix - make action icons on movie card configurable.

--- movies/src/components/movieCard/index.js
+++ movies/src/components/movieCard/index.js
@@ -1,4 +1,4 @@
-import React, { useContext  } from "react";
+import React, { useContext } from "react";
 import { MoviesContext } from "../../contexts/moviesContext";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
@@ -16,19 +16,19 @@ import { Link } from "react-router-dom";
 import Avatar from '@mui/material/Avatar';
 import img from '../../images/film-poster-placeholder.png'
 
-export default function MovieCard({ movie }) {
+export default function MovieCard({ movie, action }) {
     const { favorites, addToFavorites } = useContext(MoviesContext);
 
-  if (favorites.find((id) => id === movie.id)) {
-    movie.favorite = true;
-  } else {
-    movie.favorite = false
-  }
+    if (favorites.find((id) => id === movie.id)) {
+        movie.favorite = true;
+    } else {
+        movie.favorite = false
+    }
 
-  const handleAddToFavorite = (e) => {
-    e.preventDefault();
-    addToFavorites(movie);
-  };
+    const handleAddToFavorite = (e) => {
+        e.preventDefault();
+        addToFavorites(movie);
+    };
 
 
 
@@ -73,9 +73,7 @@ export default function MovieCard({ movie }) {
                 </Grid>
             </CardContent>
             <CardActions disableSpacing>
-                <IconButton aria-label="add to favorites" onClick={handleAddToFavorite}>
-                    <FavoriteIcon color="primary" fontSize="large" />
-                </IconButton>
+                {action(movie)}
                 <Link to={`/movies/${movie.id}`}>
                     <Button variant="outlined" size="medium" color="primary">
                         More Info ...
